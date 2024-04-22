3c15e42c120fca6ebf2ebfb3fff0d5ae1b4fdcbe
CommnetEnterprises <96027891+lettimaks@users.noreply.github.com>
2024-04-22 10:43:41 +0200

    fixes

--- spin_dir/script/script.js
+++ spin_dir/script/script.js
@@ -1,17 +1,24 @@
 const $map = document.querySelector(".map");
 const $nodes = document.querySelectorAll(".node");
 const $nodes1 = document.querySelectorAll(".node1");
-const $introModal1 = document.getElementById("introModal1");
+const $introModal2 = document.getElementById("introModal2");
 const $introModal = document.getElementById("introModal");
 
 const $introModalBackground = document.getElementById("introModalBackground");
 const $introModalClose = document.getElementById("introModalClose");
 const $introModalTitle = document.getElementById("introModalTitle");
 const $introModalDescription = document.getElementById("introModalDescription");
-const $introModalDescription2 = document.getElementById("introModalDescription2");
 const $introModalIcon = document.getElementById("introModalIcon");
 const $introModalPrev = document.getElementById("introModalPrev");
 const $introModalNext = document.getElementById("introModalNext");
+
+const $introModalBackground2 = document.getElementById("introModalBackground2");
+const $introModalClose2 = document.getElementById("introModalClose2");
+const $introModalTitle2 = document.getElementById("introModalTitle2");
+const $introModalDescription2 = document.getElementById("introModalDescription2");
+const $introModalIcon2 = document.getElementById("introModalIcon2");
+const $introModalPrev2 = document.getElementById("introModalPrev2");
+const $introModalNext2 = document.getElementById("introModalNext2");
 
 let moduleData = [];
 let moduleData2 = [];
@@ -26,9 +33,9 @@ let maxIndex = 11;
   });
 });
 
-[$introModalBackground, $introModalClose].forEach((elem) => {
+[$introModalBackground2, $introModalClose2].forEach((elem) => {
   elem.addEventListener("click", () => {
-    $introModal1.classList.remove("is-active");
+    $introModal2.classList.remove("is-active");
   });
 });
 
@@ -36,7 +43,15 @@ $introModalPrev.addEventListener("click", () => {
   showModal(currentIndex > 0 ? currentIndex - 1 : maxIndex);
 });
 
+$introModalPrev2.addEventListener("click", () => {
+  showModal(currentIndex > 0 ? currentIndex - 1 : maxIndex);
+});
+
 $introModalNext.addEventListener("click", () => {
+  showModal(currentIndex < maxIndex ? currentIndex + 1 : 0);
+});
+
+$introModalNext2.addEventListener("click", () => {
   showModal(currentIndex < maxIndex ? currentIndex + 1 : 0);
 });
 
@@ -51,7 +66,7 @@ $nodes.forEach(($node) => {
   });
 });
 
-$nodes.forEach(($node1) => {
+$nodes1.forEach(($node1) => {
   // Store module data in array
   moduleData2[$node1.dataset.index] = JSON.parse($node1.dataset.info);
 
@@ -88,19 +103,19 @@ const showModal = (index) => {
 
 const showModal2 = (index) => {
   currentIndex = Number(index);
-  const data = moduleData[currentIndex];
+  const data = moduleData2[currentIndex];
 
-  $introModalTitle.innerText = data.subtitle || data.title;
-  $introModalDescription.src = `../../assets/data/${data.image}`;
+  $introModalTitle2.innerText = data.subtitle || data.title;
+  $introModalDescription2.src = `../../assets/data/${data.image}`;
   $introModalDescription2.src = `../../assets/data/${data.image}`;
   
-  $introModalIcon.className = `fas fa-${data.icon} fa-8x`;
-  $introModal.classList.add("is-active");
+  $introModalIcon2.className = `fas fa-${data.icon} fa-8x`;
+  $introModal2.classList.add("is-active");
 
-  if (isOverflowing($introModalDescription)) {
-    $introModalDescription.classList.add("is-overflowing");
+  if (isOverflowing($introModalDescription2)) {
+    $introModalDescription2.classList.add("is-overflowing");
   } else {
-    $introModalDescription.classList.remove("is-overflowing");
+    $introModalDescription2.classList.remove("is-overflowing");
   }
 
   if (isOverflowing($introModalDescription2)) {
