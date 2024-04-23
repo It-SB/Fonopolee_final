3fac28650b4c2500c8da03d0be9f424a7b23faa1
CommnetEnterprises <155953018+It-SB@users.noreply.github.com>
2024-04-23 13:01:13 +0200

    test root change1

--- spin_dir/script/script.js
+++ spin_dir/script/script.js
@@ -81,7 +81,7 @@ const showModal = (index) => {
   const data = moduleData[currentIndex];
 
   $introModalTitle.innerText = data.subtitle || data.title;
-  $introModalDescription.src = `../assets/data/${data.image}`;
+  $introModalDescription.src = `.../../assets/data/${data.image}`;
   // $introModalDescription2.src = `.../assets/data/${data.image}`;
 
   $introModalIcon.className = `fas fa-${data.icon} fa-8x`;
@@ -100,7 +100,7 @@ const showModal2 = (index) => {
   const data = moduleData2[currentIndex];
 
   $introModalTitle2.innerText = data.subtitle || data.title;
-  $introModalDescription2.src = `.../assets/data/${data.image}`;
+  $introModalDescription2.src = `../../assets/data/${data.image}`;
 
   $introModalIcon2.className = `fas fa-${data.icon} fa-8x`;
   $introModal2.classList.add("is-active");
