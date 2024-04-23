b52247c6684ed84117fc5ec62c1744360f6a6e07
CommnetEnterprises <155953018+It-SB@users.noreply.github.com>
2024-04-23 12:54:48 +0200

    changing popups

--- spin_dir/script/script.js
+++ spin_dir/script/script.js
@@ -81,7 +81,7 @@ const showModal = (index) => {
   const data = moduleData[currentIndex];
 
   $introModalTitle.innerText = data.subtitle || data.title;
-  $introModalDescription.src = `.../assets/data/${data.image}`;
+  $introModalDescription.src = `../assets/data/${data.image}`;
   // $introModalDescription2.src = `.../assets/data/${data.image}`;
 
   $introModalIcon.className = `fas fa-${data.icon} fa-8x`;
